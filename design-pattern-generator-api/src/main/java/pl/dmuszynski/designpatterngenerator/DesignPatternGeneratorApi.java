3942b8ac9e80806e7bb090606ba921c47bc380fa
Damian Muszyński <DMuszynski>
2022-03-26 16:38:00 +0100

    first commit

---
+++ design-pattern-generator-api/src/main/java/pl/dmuszynski/designpatterngenerator/DesignPatternGeneratorApi.java
@@ -0,0 +1,11 @@
+package pl.dmuszynski.designpatterngenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternGeneratorApi {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternGeneratorApi.class, args);
+    }
+}
